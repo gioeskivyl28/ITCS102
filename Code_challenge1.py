11f0f257104c2c10327afb99caa9efd7d9b6cbf6
Gio Esquivel <gioboyesquivel123@gmail.com>
2026-08-21 19:32:31 +0800

    Add files via upload

---
+++ Code_challenge1.py
@@ -0,0 +1,2 @@
+code = "\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\tHI MY NAME IS\t\t\t\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\"GIO ESQUIVEL\"\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*"
+print (code)
